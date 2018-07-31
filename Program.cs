de40340b75802c39508640bd091338f7f6fc94d0
kisekitw <kisekitw@gmail.com>
2018-07-31 23:01:20 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using ElectronNET.API;
+
+namespace ElectronNET_API_Demos
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseElectron(args)
+                .Build();
+    }
+}
